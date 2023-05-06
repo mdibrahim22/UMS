49823911ccd41d915cf32a9e852834815df522a8
Md Ibrahim Hossain <md.ibhw@gmail.com>
2023-05-06 21:43:56 +0600

    University Management System

---
+++ src/test/java/me/mdibrahim/UMS/UmsApplicationTests.java
@@ -0,0 +1,13 @@
+package me.mdibrahim.UMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
